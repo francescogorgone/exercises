d08487ec096ff26efa2d6303087af51e1b9c142b
Francesco Gorgone <148762861+francescogorgone@users.noreply.github.com>
2023-10-24 12:29:21 +0200

    Create exponentiation.py

---
+++ Homework/exponentiation.py
@@ -0,0 +1,15 @@
+print("------------------------------------------------\n\n\n")
+print("")
+print("")
+
+a = int(input("dammi un numero: "))
+print(a)
+
+b = int(input("dammi un altro numero: "))
+print(b)
+
+c = a ** b
+
+print("risultato: " + str(c))
+
+
