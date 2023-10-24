72e3b27d6cea5352218e491f6e00ecf1f6be6f15
Francesco Gorgone <148762861+francescogorgone@users.noreply.github.com>
2023-10-24 12:40:31 +0200

    Create homework_2.1.py

---
+++ Homework/homework_2.1.py
@@ -0,0 +1,16 @@
+dic = {"name" : "francesco" , "nickname" : "grg"}
+
+print(dic["name"])
+
+##compito1
+
+#dichiarare due liste di numeri con 5 elementi ciascuna e creare una lista concatenata che le includa entrambe
+
+list1 = [0, 1, 2, 3, 4]
+list2 = [5, 6, 7, 8, 9]
+
+list_tot = [list1 + list2]
+
+print(list_tot)
+
+#bonus: prendere gli elementi delle due liste dall'utente chiedendoli uno per uno
