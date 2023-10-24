a436e81d14eadc8d2648aa7990f54c1376b8a4f2
Francesco Gorgone <148762861+francescogorgone@users.noreply.github.com>
2023-10-24 12:33:11 +0200

    Create homework_2.py

---
+++ Homework/homework_2.py
@@ -0,0 +1,19 @@
+#dizionari e liste
+
+
+
+
+#dichiarere 2 liste di numeri con 5 elementi ciascuna
+#creare una lista concatenata che le includa entrambi
+
+#utilizza un loop
+
+list1 = [0, 1, 2, 3, 4]
+list2 = [5, 6, 7, 8, 9]
+
+list3 = [list1, list2]
+
+for i in list3:
+    print(i)
+
+print(list3)
