6f56b0afea47c76bdec0afad172e996865649b40
Francesco Gorgone <148762861+francescogorgone@users.noreply.github.com>
2024-07-03 22:35:22 +0200

    Create condizioni_matematiche.py

---
+++ Lesson_3/condizioni_matematiche.py
@@ -0,0 +1,10 @@
+#Scrivere una condizione che sia vera se x e y
+#sono uguali oppure uno è il doppio dell'altro
+
+x = 5
+y = 10
+
+if x == y or x == 2*y or y == 2*x:
+  print("Bravo")
+else:
+  print("Unlucky")
